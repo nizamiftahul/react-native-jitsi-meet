78113b319e91086138422fc1b502a613b088a70d
Diego Mello <diegolmello@gmail.com>
2021-08-11 16:21:19 -0300

    Fix Android black screen by adding "options" prop to JitsiMeetView

--- android/src/main/java/com/reactnativejitsimeet/RNJitsiMeetViewManager.java
+++ android/src/main/java/com/reactnativejitsimeet/RNJitsiMeetViewManager.java
@@ -2,19 +2,20 @@ package com.reactnativejitsimeet;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.common.MapBuilder;
+import com.facebook.react.module.annotations.ReactModule;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.annotations.ReactProp;
 import com.facebook.react.uimanager.events.RCTEventEmitter;
-import com.facebook.react.module.annotations.ReactModule;
 
 import org.jitsi.meet.sdk.JitsiMeetViewListener;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Map;
-
-import static java.security.AccessController.getContext;
 
 @ReactModule(name = RNJitsiMeetViewManager.REACT_CLASS)
 public class RNJitsiMeetViewManager extends SimpleViewManager<RNJitsiMeetView> implements JitsiMeetViewListener {
@@ -40,6 +41,60 @@ public class RNJitsiMeetViewManager extends SimpleViewManager<RNJitsiMeetView> i
             mJitsiMeetViewReference.setJitsiMeetView(view);
         }
         return mJitsiMeetViewReference.getJitsiMeetView();
+    }
+
+    @ReactProp(name="options")
+    public void setProps(RNJitsiMeetView jitsiMeetView,  ReadableMap options) throws MalformedURLException {
+
+        if(mJitsiMeetViewReference.getJitsiMeetView() != null) {
+            mJitsiMeetViewReference.getJitsiMeetView().leave();
+            mJitsiMeetViewReference.getJitsiMeetView().dispose();
+        }
+
+        String url = null;
+        boolean audioOnly = false;
+
+        if (mJitsiMeetViewReference.getJitsiMeetView() != null) {
+
+            RNJitsiMeetUserInfo _userInfo = new RNJitsiMeetUserInfo();
+            if (options != null) {
+                if (options.hasKey("url") && options.getString("url") != null) {
+                    url = options.getString("url");
+                }
+                if (options.hasKey("audioOnly")) {
+                    if (options.getBoolean("audioOnly") || !options.getBoolean("audioOnly")) {
+                        audioOnly = options.getBoolean("audioOnly");
+                    }
+                }
+                if (options.hasKey("userInfo")) {
+                    ReadableMap user = options.getMap("userInfo");
+                    if (user.hasKey("displayName")) {
+                        _userInfo.setDisplayName(user.getString("displayName"));
+                    }
+                    if (user.hasKey("email")) {
+                        _userInfo.setEmail(user.getString("email"));
+                    }
+                    if (user.hasKey("avatar")) {
+                        String avatarURL = user.getString("avatar");
+                        try {
+                            _userInfo.setAvatar(new URL(avatarURL));
+                        } catch (MalformedURLException e) {
+                        }
+                    }
+                }
+            }
+
+            RNJitsiMeetConferenceOptions jitsiOptions
+                    = new RNJitsiMeetConferenceOptions.Builder()
+                    .setRoom(url)
+                    .setUserInfo(_userInfo)
+                    .setFeatureFlag("chat.enabled", false)
+                    .setFeatureFlag("pip.enabled", false)
+                    .setAudioOnly(audioOnly)
+                    .build();
+
+            mJitsiMeetViewReference.getJitsiMeetView().join(jitsiOptions);
+        }
     }
 
     public void onConferenceJoined(Map<String, Object> data) {
